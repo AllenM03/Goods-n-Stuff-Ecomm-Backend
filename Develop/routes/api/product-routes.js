a7c4bac95845d5baba3de509ec8ef989dbe474e0
Allen McBroom Bey <98724344+AllenM03@users.noreply.github.com>
2022-07-04 03:12:34 -0400

    adds await functions and try..catch error handling

--- Develop/routes/api/product-routes.js
+++ Develop/routes/api/product-routes.js
@@ -4,15 +4,32 @@ const { Product, Category, Tag, ProductTag } = require('../../models');
 // The `/api/products` endpoint
 
 // get all products
-router.get('/', (req, res) => {
+router.get('/', async(req, res) => {
   // find all products
   // be sure to include its associated Category and Tag data
+    try {
+    const productData = await Product.findAll({
+      include: [Category, {model: Tag, through: ProductTag}]
+    })
+    res.status(200).json(productData)
+  } catch (err) {
+    res.status(500).json(err)
+  }
 });
 
+
 // get one product
-router.get('/:id', (req, res) => {
+router.get('/:id', async (req, res) => {
   // find a single product by its `id`
   // be sure to include its associated Category and Tag data
+  try {
+    const productData = await Product.findByPk(req.params.id, {
+      include: [Category, {model: Tag, through: ProductTag}]
+    })
+    res.status(200).json(productData)
+  } catch (err) {
+    res.status(500).json(err)
+  }
 });
 
 // create new product
@@ -89,8 +106,18 @@ router.put('/:id', (req, res) => {
     });
 });
 
-router.delete('/:id', (req, res) => {
+router.delete('/:id', async (req, res) => {
   // delete one product by its `id` value
+  try {
+    const productData = await Product.delete( {
+      where: {
+        id:req.params.id
+      }
+    })
+    res.status(200).json(productData);
+    } catch (err) {
+      res.status(500).json(err)
+    }
 });
 
 module.exports = router;
