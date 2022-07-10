7246a8caac4741644503ee0a588e51b0a344b025
Allen <almcbroombey@hotmail.com>
2022-07-10 04:23:04 -0400

    defined columns information

--- Develop/models/Tag.js
+++ Develop/models/Tag.js
@@ -5,8 +5,18 @@ const sequelize = require('../config/connection.js');
 class Tag extends Model {}
 
 Tag.init(
-  {
-    // define columns
+  { 
+    ////// define columns /////
+    
+    id: {
+       type: DataTypes.INTEGER,
+       allowNull: false,
+       primaryKey: true,
+       autoIncrement: true,
+    },
+    tag_name: {
+      type: DataTypes.STRING
+    }
   },
   {
     sequelize,
@@ -16,5 +26,6 @@ Tag.init(
     modelName: 'tag',
   }
 );
+
 
 module.exports = Tag;
--- Develop/routes/api/product-routes.js
+++ Develop/routes/api/product-routes.js
@@ -34,14 +34,7 @@ router.get('/:id', async (req, res) => {
 
 // create new product
 router.post('/', (req, res) => {
-  /* req.body should look like this...
-    {
-      product_name: "Basketball",
-      price: 200.00,
-      stock: 3,
-      tagIds: [1, 2, 3, 4]
-    }
-  */
+  
   Product.create(req.body)
     .then((product) => {
       // if there's product tags, we need to create pairings to bulk create in the ProductTag model
